183284737617e002ddc1ac01224e426f55b48529
gima <gimantha.k98@gmail.com>
2020-06-10 13:44:02 +0530

    started css for homepage

---
+++ Pages/homepage.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta content="text/html; charset=utf-8">
+<title>Welcome to Bikubiworld</title>
+<link rel="stylesheet" href="../Resources/CSS/grid.css">
+<link rel="stylesheet" href="../Resources/CSS/normalize.css">
+<link rel="stylesheet" href="../Stylesheets/mainStyles.css">
+<link rel="stylesheet" href="../Stylesheets/navbarStyle.css">
+<link rel="stylesheet" href="../Stylesheets/homestyle.css">
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;1,300&display=swap" rel="stylesheet">
+
+</head>
+<body>
+        <header>
+            <nav>
+                <div class="row">
+                    <img src="../Resources/assets/BKB.JPG" alt="BKB logo" class="logo">
+                    <ul class="main-nav">
+                        <li><a href="#">Food delivery</a></li>
+                        <li><a href="#">How it works</a></li>
+                        <li><a href="#">Our cities</a></li>
+                        <li><a href="#">Sign up</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+  
+        <section class="section-features">
+            <div class="row">
+                <h2>Get food fast &mdash; not fast food</h2>
+                <p class="long-copy">
+                    Hello, we’re Bikubiworld, your new premium food delivery service. We know you’re always busy. No time for cooking. So let us take care of that, we’re really good at it, we promise!
+                </p>
+            </div>
+            <div class="row">
+            <h3>DISCOVER<br>COLLECTIONS</h3>
+            </div>
+            <div class="row">
+              <div class= "col span-1-of-2">
+                <img src="../Resources/assets/babyyyyyyyyyyy.png" width="auto" height="500" alt="kids_toys">
+              </div>
+              <div class= "col span-2-of-2">
+                <img src="../Resources/assets/boys toys.jpg" width="auto" height="250" alt="boys_toy">
+                <img src="../Resources/assets/baby.jpg" width="auto" height="250">
+              </div>
+            </div>
+        </section>
+
+        <section class="section-products">
+        <h3>POPULAR<br>PRODUCTS</h3>
+  
+            <p><br>
+            </p>
+          <table width="750">
+            <tr>
+            <td width="176"><img src="../assets/tedd.jpg" width="250" height="303" /></td>
+            <td width="179"><img src="../assets/bat.jpg" width="250" height="303" /></td>
+            <td width="123"><img src="../assets/remote car.png" width="250" height="303" /></td>
+            <td width="123"><img src="../assets/remote car.png" width="250" height="303" /></td>
+          </tr>
+          <tr>
+            <td><h2>Wool Teddy bear</h2>
+              <p>$20.00</p>
+              <input name="btnAdd" type="submit" value="Add to cart" /></td>
+            <td><h2>Cricket bat</h2>
+              <p>$75.00</p>
+              <input name="btnAdd" type="submit" value="Add to cart" /></td>
+            <td><h2>Remote car</h2>
+              <p>$60.00</p>
+              <input name="btnAdd" type="submit" value="Add to cart" /></td>
+            <td><h2>Remote car</h2>
+              <p>$60.00</p>
+              <input name="btnAdd" type="submit" value="Add to cart" /></td>
+          </tr>
+          </table>
+        </section>
+
+
+
+
+
+
+  
+
+
+<h1>WATCH THEM GROW!!<h1>
+<img src="../assets/cover11.jpg" width="1000" height="400">
+
+</body>
+</html>
