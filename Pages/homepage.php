e7fb0669fca9f8f5461bd6e4abde8cdb4fae8e13
gima <gimantha.k98@gmail.com>
2020-06-10 22:13:52 +0530

    modified homepage

--- Pages/homepage.php
+++ Pages/homepage.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +11,7 @@
 <link rel="stylesheet" href="../Stylesheets/mainStyles.css">
 <link rel="stylesheet" href="../Stylesheets/navbarStyle.css">
 <link rel="stylesheet" href="../Stylesheets/homestyle.css">
+<link rel="stylesheet" type="text/css" href="../Resources/CSS/ionicons.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;1,300&display=swap" rel="stylesheet">
 
 </head>
@@ -34,7 +38,7 @@
                 </p>
             </div>
             <div class="row">
-            <h3>DISCOVER<br>COLLECTIONS</h3>
+            <h2>DISCOVER COLLECTIONS</h2>
             </div>
             <div class="row">
               <div class= "col span-1-of-2">
@@ -47,45 +51,48 @@
             </div>
         </section>
 
-        <section class="section-products">
-        <h3>POPULAR<br>PRODUCTS</h3>
-  
-            <p><br>
-            </p>
-          <table width="750">
-            <tr>
-            <td width="176"><img src="../assets/tedd.jpg" width="250" height="303" /></td>
-            <td width="179"><img src="../assets/bat.jpg" width="250" height="303" /></td>
-            <td width="123"><img src="../assets/remote car.png" width="250" height="303" /></td>
-            <td width="123"><img src="../assets/remote car.png" width="250" height="303" /></td>
-          </tr>
-          <tr>
-            <td><h2>Wool Teddy bear</h2>
-              <p>$20.00</p>
-              <input name="btnAdd" type="submit" value="Add to cart" /></td>
-            <td><h2>Cricket bat</h2>
-              <p>$75.00</p>
-              <input name="btnAdd" type="submit" value="Add to cart" /></td>
-            <td><h2>Remote car</h2>
-              <p>$60.00</p>
-              <input name="btnAdd" type="submit" value="Add to cart" /></td>
-            <td><h2>Remote car</h2>
-              <p>$60.00</p>
-              <input name="btnAdd" type="submit" value="Add to cart" /></td>
-          </tr>
-          </table>
+        <section class='section-products'>
+        <div class='row'>
+          <h2>POPULAR PRODUCTS</h2>
+        </div>
+        <div class='row'>
+          <?php
+          $con = mysqli_connect("localhost","root","","bikubiworld");
+          if(!$con)
+            {
+             die("cannot connect to DB server");
+            }
+            $sql="SELECT * FROM `product` WHERE `status`='active';";
+            $rowSQL= mysqli_query($con,$sql);
+            $i=4;
+            while(($row = mysqli_fetch_array($rowSQL)) && $i>0 ){
+              echo"
+              <div class='col span-1-of-4 box'>
+                    <img src='".$row['pimage']."' alt='pimage'>
+                    <h3>".$row['pname']."</h3>
+                    <div class='product-feature'>
+                    <i class='ion-ios-person icon-small'></i>
+                    ".$row['pcategory']."
+                    </div>
+                    <div class='product-feature'>
+                    <i class='ion-ios-star icon-small'></i>
+                    $".$row['pprice']."
+                    </div>
+                    <div class='product-feature'>
+                    <i class='ion-social-twitter icon-small'></i>
+                    Available now!
+                    </div>
+              </div>";
+              $i--;
+            }
+            ?>
+        </div>
         </section>
-
-
-
-
-
-
-  
-
-
-<h1>WATCH THEM GROW!!<h1>
-<img src="../assets/cover11.jpg" width="1000" height="400">
-
+    <div class='row'>
+    <h2>WATCH THEM GROW!!</h2>
+    </div>
+    <div class='row'>
+    <img src="../Resources/assets/cover11.jpg" width="1000" height="400">
+    </div>
 </body>
 </html>
